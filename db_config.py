f4742e81a551f8447a9520e38afea20d7994c23f
pavxxx <pavithra020906@gmail.com>
2026-03-15 12:00:41 +0530

    added my changes

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Vinu@2005",
+        password="Pavxx@02",
         database="fide"
     )
