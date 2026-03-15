b6604ec72e5d6bbe7e9ebe07c2970200ec9ed5e2
gawthamprasathvp <gawthamprasathvp@gmail.com>
2026-03-15 10:26:48 +0530

    Initial commit - FIDE DBMS project

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Pavxx@02",
+        password="Vinu@2005",
         database="fide"
     )
